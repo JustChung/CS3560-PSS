d21e2f8293de78653819f07c139bc2025e6f5b33
ZombiMigz <chunyumikey@gmail.com>
2024-05-06 14:23:57 -0700

    fixes "more"

--- src/views/CalendarView.tsx
+++ src/views/CalendarView.tsx
@@ -1,9 +1,9 @@
-import { Whisper, Popover, Calendar } from "rsuite";
+import { Calendar } from "rsuite";
 import PSSController from "../classes/PSSController";
-import { useState, useCallback } from "react";
+import { useState, useCallback, ReactElement } from "react";
 import { getTime } from "../utils";
 import { useForceRerender } from "../hooks/useForceRerender";
-import { Chip, ChipOwnProps, Stack, Typography } from "@mui/material";
+import { Button, Chip, ChipOwnProps, Popover, Stack, Typography } from "@mui/material";
 import Task from "../classes/Task";
 import { TransientTask } from "../classes/TransientTask";
 import { AntiTask } from "../classes/AntiTask";
@@ -28,6 +28,54 @@ const getTaskColor = (task: TransientTask | RecurringTask, tasks: Task[]): ChipO
 
   // Uncancelled recurring tasks
   return "primary";
+};
+
+const TaskChip = ({ task, tasks }: { task: TransientTask | RecurringTask; tasks: Task[] }) => {
+  return (
+    <Chip
+      variant='outlined'
+      color={getTaskColor(task, tasks)}
+      size='small'
+      sx={{ display: "flex", justifyContent: "flex-start" }}
+      label={
+        <Typography variant='body2' noWrap>
+          <b>{getTime(task.startTime)}</b> - {task.name}
+        </Typography>
+      }
+    />
+  );
+};
+
+const MoreTasks = ({ tasks, text }: { tasks: (TransientTask | RecurringTask)[]; text: string }) => {
+  const [anchor, setAnchor] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchor(event.currentTarget);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setAnchor(null);
+  }, []);
+
+  return (
+    <>
+      <Button variant='text' onClick={handleClick} size='small' fullWidth sx={{ fontSize: 11 }}>
+        {text}
+      </Button>
+      <Popover
+        open={Boolean(anchor)}
+        onClose={handleClose}
+        anchorEl={anchor}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}>
+        {tasks.map((task, index) => (
+          <TaskChip task={task} tasks={tasks} key={index} />
+        ))}
+      </Popover>
+    </>
+  );
 };
 
 export const CalendarView = ({ controller }: { controller: PSSController }) => {
@@ -60,39 +108,12 @@ export const CalendarView = ({ controller }: { controller: PSSController }) => {
 
       const moreCount = displayTasks.length > 2 ? displayTasks.length - 2 : 0;
       if (displayTasks.length) {
-        const taskItems = (
-          <li>
-            <Whisper
-              placement='top'
-              trigger='click'
-              speaker={
-                <Popover>
-                  {tasks.map((task, index) => (
-                    <p key={index}>
-                      <b>{task.startTime}</b> - {task.name}
-                    </p>
-                  ))}
-                </Popover>
-              }>
-              <a>{moreCount} more</a>
-            </Whisper>
-          </li>
-        );
+        const taskItems = <MoreTasks tasks={displayTasks.slice(2)} text={moreCount + " more..."} />;
 
         return (
           <Stack>
             {displayTasks.slice(0, 2).map((task, index) => (
-              <Chip
-                variant='outlined'
-                color={getTaskColor(task, tasks)}
-                size='small'
-                key={index}
-                sx={{ display: "flex", justifyContent: "flex-start" }}
-                label={
-                  <Typography variant='body2' key={index} noWrap>
-                    <b>{getTime(task.startTime)}</b> - {task.name}
-                  </Typography>
-                }></Chip>
+              <TaskChip task={task} key={index} tasks={tasks} />
             ))}
             {moreCount ? taskItems : null}
           </Stack>
