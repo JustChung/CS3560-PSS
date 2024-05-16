c3a9037ca15b89d08a2e8e02381cf835afb3d64a
loki1001 <lokmun465@gmail.com>
2024-05-15 22:37:51 -0700

    Added comments to PSSController

--- src/classes/PSSController.ts
+++ src/classes/PSSController.ts
@@ -11,6 +11,7 @@ export default class PSSController {
     this.pss = pss;
   }
 
+  // Method to add task to schedule
   addTask(
     name: string,
     taskClass: "transient" | "anti" | "recurring",
@@ -21,43 +22,56 @@ export default class PSSController {
     endDate?: number,
     frequency?: Frequency
   ): true | string {
+    // Check if task name is unique
     if (!this.pss.verifyUniqueName(name)) {
       return `A task with the name "${name}" already exists.`;
     }
+
+    // Verify that the start date is valid
     const validDate = this.pss.verifyValidDate(startDate);
     if (validDate !== true) {
       return validDate;
     }
+
+    // Check for overlapping tasks
     const noOverlap = this.pss.verifyNoOverlap(taskClass, startDate, startTime, duration);
     if (noOverlap === true) {
+      // Create task if there are no overlaps
       this.pss.createTask(name, taskClass, startTime, startDate, duration, taskType, endDate, frequency);
     } else {
+      // Return error if there are overlaps
       return noOverlap;
     }
 
-    return true;
+    return true; // Return true if task added successfully
   }
 
+  // Method to print all tasks in the schedule
   printTasks(): void {
     this.pss.printTasks();
   }
   
+  // Method to delete a task from the schedule by name
   deleteTask(name: string): string | void {
     return this.pss.deleteTask(name);
   }
 
+  // Method to view details of a task by name
   viewTask(name: string): Task | undefined {
     return this.pss.getTask(name);
   }
 
+  // Method to view schedule for the specified date range
   viewSchedule(startDate: number, type: "day" | "week" | "month" | "calendar"): Task[] {
     return this.pss.getSchedule(startDate, type);
   }
 
+  // Method to wrte schedule to file
   writeScheduleToFile(fileName: string, startDate: number, type: "whole" | "day" | "week" | "month"): void {
     this.pss.writeScheduleToFile(fileName, startDate, type);
   }
 
+  // Method to read schedule data from file and update schedule
   inputFileToSchedule(fileName: File): void {
     this.pss.readScheduleFromFile(fileName);
   }
